8f75dc910e3ed3c55fb875fe2583c693b8f42954
Jakub Stachurski <j.stachurski@student.utwente.nl>
2024-12-06 22:34:31 +0100

    Put comments on iterators

--- src/iter.lua
+++ src/iter.lua
@@ -1,9 +1,14 @@
 local util = require("src.utils")
 
---- @class Iter 
---- @field fn fun(): (integer,any) 
-Iter = {} 
+--- @generic T
+--- @class Iter<T>  
+--- @field fn fun(): integer|nil,unknown|nil
+Iter = {}
 
+--- Creates an iterator from a function that returns index and T or nil  
+---@generic T
+---@param fn fun(): (integer|nil, T|nil)
+---@return Iter<T>
 function Iter:create(fn)
   local out = {fn = fn}
   self.__index = self 
@@ -11,6 +16,10 @@ function Iter:create(fn)
 end
 
 
+--- Creates an iterator from a function that returns T or nil  
+---@generic T
+---@param fn fun(): (T)
+---@return Iter<T>
 function Iter:wrap(fn) 
   if type(fn) == 'table' and getmetatable(fn) == Iter then 
     return fn    
@@ -25,6 +34,11 @@ function Iter:wrap(fn)
   return Iter:create(f)
 end 
 
+---Creates a iterator that returns element `v` `n` times 
+---@generic T 
+---@param v T 
+---@param n integer
+---@return Iter<T>
 function Iter:rep(v,n) 
   assert(v, "v is nil")
   assert(type(assert(n, "n is null")) == 'number', "n is not integer")
@@ -37,20 +51,24 @@ function Iter:rep(v,n)
   return Iter:create(f)
 end  
 
-function Iter:list_append(t) 
-  self.append_app = self:map(function(x) t[#t +1] = x return 1 end):run_out()
-end 
 
+
+---Returns all the elements in the iterator
+---@generic T 
+---@return T[]
 function Iter:collect()
   local out = {} 
   local i,x = self.fn() 
-  while x ~= nil do 
+  while x ~= nil and i ~= nil do 
     out[i] = x  
     i,x = self.fn()
   end
   return out
 end 
 
+
+--- Drains all the iterator, discarding the value 
+---@generic T 
 function Iter:run_out()
   local i,_ = self.fn()
   while i ~= nil do
@@ -58,10 +76,23 @@ function Iter:run_out()
   end
 end 
 
+---Appends a array with elements from the iterator   
+---@generic T 
+---@param t T[] 
+function Iter:list_append(t) 
+  ---@diagnostic disable-next-line: undefined-field
+  self.append_app = self:map(function(x) t[#t +1] = x return 1 end):run_out()
+end 
+
+--- Returns a iterator that executes `fn` on each of the elements of the original Iter
+---@generic T 
+---@generic M  
+---@param fn fun(T): M| nil  
+---@return Iter<M>
 function Iter:map(fn) 
   local map = Iter:create(function ()  
     local i,x = self.fn()
-    if x ~= nil then 
+    if x ~= nil and i ~= nil then 
      return i,fn(x) 
     end 
     return nil
@@ -69,6 +100,10 @@ function Iter:map(fn)
   return map 
 end  
 
+--- Returns an iterator of elements x from the original iterator for which fn(x) is true
+---@generic T 
+---@param fn fun(T): boolean   
+---@return Iter<T>
 function Iter:filter(fn)
   local i = 0 
   local filter = Iter:create(function() 
@@ -86,6 +121,10 @@ function Iter:filter(fn)
   return filter 
 end 
 
+--- Returns the first element x in the original iterator for which fn(x) is true
+---@generic T 
+---@param fn fun(T): boolean   
+---@return T|nil
 function Iter:find(fn) 
   local _,x = self.fn()
   while x ~= nil do
@@ -98,6 +137,12 @@ function Iter:find(fn)
   
 end 
 
+--- Returns an value based on the following formula Fold(i + 1) = fn(Fold(i), Iter(i)) where Fold(0) = init 
+---@generic T 
+---@generic R 
+---@param init R 
+---@param fn fun(R,T): R
+---@return R
 function Iter:fold(init, fn) 
   for _,x in self.fn do
     init = fn(init,x)
@@ -106,10 +151,14 @@ function Iter:fold(init, fn)
 end 
 
 
+--- Returns the sum of the iterator, requires the iterator to be Iter<number>
+---@return number
 function Iter:sum() 
   return self:fold(0,function (i, x)  return i + x end)
 end
 
+--- Returns the concatenation of all strings in the iterator, requires the iterator to be Iter<string>
+---@return string
 function Iter:concat()
   return self:fold("",function (i, x)  return i .. x end)
 end 
@@ -117,13 +166,20 @@ function Iter:stitch_lines()
   return self:fold("",function (i, x)  return i .. x .. "\n" end)
 end 
 
----@param it Iter 
----@return Iter
+---Appends one iterator to another 
+---@generic T 
+---@param it Iter<T> 
+---@return Iter<T>
 function Iter:append(it) 
+  ---@type integer|nil
   local i = 0 
+  ---@type integer|nil
   local i1 = 0 
+  ---@type integer|nil
   local i2 = 0 
   local first = true
+  ---@generic T  
+  ---@type T|nil 
   local v = 0
   local function f()
     if first then 
@@ -134,7 +190,7 @@ function Iter:append(it)
       end  
       first = false
     end 
-    i2,v = it:fn()
+    i2,v = it.fn()
   
     if i2 == nil then 
       return nil,nil
@@ -146,7 +202,9 @@ function Iter:append(it)
 end 
 
 --- comment
---- @param fn fun(any): Iter
+--- @generic T 
+--- @generic P 
+--- @param fn fun(T): Iter<P>
 function Iter:product(fn) 
   local i = 0 
   local _,v0 = self.fn()
@@ -171,6 +229,9 @@ function Iter:product(fn)
   return Iter:create(f)
 end 
 
+--- Creates an iterator of each character in a string 
+---@param s string
+---@return Iter<string>
 function Iter:from_str(s) 
   local l = s:len()
   local i = 0 
@@ -182,11 +243,18 @@ function Iter:from_str(s)
   return Iter:create(f)
 end
 
-function Iter:range_inc(b,e,i)
+--- Creates an iterator of a integer range 
+---@param b integer begin 
+---@param e integer end 
+---@param i integer increment 
+---@return Iter<integer>
+---@overload fun(integer, integer): Iter<integer>
+function Iter:range(b,e,i)
+  local ii = i or 1 
   local x = b - 1
   local xi = 0 
   local function f() 
-    x = x + i
+    x = x + ii
     xi = xi + 1
     if x > e  then return nil end  
     return xi,x
@@ -195,12 +263,12 @@ function Iter:range_inc(b,e,i)
   return Iter:create(f)
 end
 
-function Iter:range(b,e) 
-  return Iter:range_inc(b,e,1)
-end
-
-function Stateless2iter(t, stateless) 
-  local f = stateless 
+--- Creates a iterator from an array
+--- @generic T 
+--- @param t T[] 
+--- @return Iter<T>
+function table.ipair_iter(t)
+  local f = ipairs(t) 
   local i = 0; 
   local v = 0; 
   local iter = Iter:create(function()
@@ -208,14 +276,24 @@ function Stateless2iter(t, stateless)
     return i,v
   end)
   return iter 
-end  
-
-function table.ipair_iter(t)
-  return Stateless2iter(t, ipairs(t))
 end 
 
+--- Creates a iterator from an associative table 
+--- @generic K 
+--- @generic T 
+--- @param t {[K]:T}
+--- @return Iter<[K,T]>
 function table.pair_iter(t)  
-  return Stateless2iter(t,pairs(t)) 
+  local i = 0; 
+  local v = 0; 
+  local k = nil; 
+  local iter = Iter:create(function()
+    k,v = next(t,k) 
+    i = i + 1
+    if v == nil then return nil end 
+    return i,{v,k}
+  end)
+  return iter 
 end 
 
 function Test()
